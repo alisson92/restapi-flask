55355cc92f52db51dccbb9c48878314376e49c1a
Alisson Lima <alissoncorrealima@hotmail.com>
2026-03-08 18:58:27 -0300

    test: testing for get user

--- tests/test_application.py
+++ tests/test_application.py
@@ -46,3 +46,19 @@ class TestApplication():
             response = client.post('/user', json=invalid_user)
             assert response.status_code == 400
             assert b"Invalid" in response.data
+
+    def test_get_user(self, client, valid_user, invalid_user):
+        with patch('application.app.UserModel.objects') as mock_objects:
+            mock_objects.return_value = [valid_user]
+            response = client.get('/user/%s' % valid_user["cpf"])
+            assert response.status_code == 200
+            assert response.json[0]["first_name"] == "Alisson"
+            assert response.json[0]["last_name"] == "Lima"
+            assert response.json[0]["cpf"] == "482.708.180-89"
+            assert response.json[0]["email"] == "alisson.lima@test.com.br"
+            assert response.json[0]["birth_date"] == "1992-03-10"
+
+            mock_objects.return_value = []
+            response = client.get('/user/%s' % invalid_user["cpf"])
+            assert response.status_code == 404
+            assert b"User not found!" in response.data
